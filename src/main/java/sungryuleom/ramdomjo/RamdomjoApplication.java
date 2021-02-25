acc2c26f15f5b7c0f66f8f3b399dc656dce938cb
sungryul.eom <sungryul.eom@timf.co.kr>
2021-02-25 23:48:52 +0900

    Initial commit

---
+++ src/main/java/sungryuleom/ramdomjo/RamdomjoApplication.java
@@ -0,0 +1,13 @@
+package sungryuleom.ramdomjo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RamdomjoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RamdomjoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sungryuleom/ramdomjo/RamdomjoApplicationTests.java
@@ -0,0 +1,13 @@
+package sungryuleom.ramdomjo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RamdomjoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
